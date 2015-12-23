792daab72aa81c3f909b772cc80b7fd82753858d
hf <fuhi1983@gmail.com>
2015-12-23 18:28:53 +0900

    fix test case

--- SampleTravisCiTests/SampleTravisCiTests.swift
+++ SampleTravisCiTests/SampleTravisCiTests.swift
@@ -22,7 +22,6 @@ class SampleTravisCiTests: XCTestCase {
     }
     
     func testExample() {
-        XCTFail()
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
